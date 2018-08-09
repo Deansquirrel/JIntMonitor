da944afa967dec6b46e74ce6a2be131c844358e6
yuansong <yuansong6@163.com>
2018-08-09 18:11:09 +0800

    201808091810 增加网页连接测试的实现（测试返回码是否为200）

--- proj/src/main/java/com/yuansong/taskjob/TaskWorkerWebState.java
+++ proj/src/main/java/com/yuansong/taskjob/TaskWorkerWebState.java
@@ -1,5 +1,7 @@
 package com.yuansong.taskjob;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -22,11 +24,49 @@ public class TaskWorkerWebState extends TaskWorkerAbstractImpl<WebStateTaskConfi
 			logger.warn("WebStateTaskConfig is null");
 			return "WebStateTaskConfig is null";
 		}
-		logger.debug(taskConfig.getUrl());
-		logger.debug(taskConfig.getMsgTitle());
-		logger.debug(taskConfig.getMsgContent());
-		logger.debug(taskConfig.getCorn());
-		return "";
+		
+		try {  
+            URL realUrl = new URL(taskConfig.getUrl());  
+            HttpURLConnection httpUrlConn = (HttpURLConnection) realUrl.openConnection();  
+   
+            httpUrlConn.setConnectTimeout(30 * 1000);
+            httpUrlConn.setReadTimeout(30 * 1000);
+            httpUrlConn.setUseCaches(false);  
+            httpUrlConn.setRequestMethod("GET");
+            
+            httpUrlConn.connect();
+            int httpCode = httpUrlConn.getResponseCode();
+            httpUrlConn.disconnect();  
+            
+            logger.debug("网页链接测试返回码 - " + String.valueOf(httpCode) + " | " + taskConfig.getUrl());
+            
+            if(httpCode == 200) {
+            	return "";
+            }
+            else {
+            	StringBuilder sb = new StringBuilder();
+            	if(!taskConfig.getMsgTitle().equals("")) {
+            		sb.append(taskConfig.getMsgTitle());
+            		sb.append("\n");
+            	}
+            	sb.append(taskConfig.getUrl());
+            	sb.append("\n");
+            	sb.append(taskConfig.getMsgContent().replaceAll("title", String.valueOf(httpCode)));
+            	return sb.toString();
+            }
+		}catch(Exception ex) {
+			ex.printStackTrace();
+			
+			StringBuilder sb = new StringBuilder();
+        	if(!taskConfig.getMsgTitle().equals("")) {
+        		sb.append(taskConfig.getMsgTitle());
+        		sb.append("\n");
+        	}
+        	sb.append(taskConfig.getUrl());
+        	sb.append("\n");
+        	sb.append("网址打开异常");
+        	return sb.toString();
+		}
 	}
 
 }
