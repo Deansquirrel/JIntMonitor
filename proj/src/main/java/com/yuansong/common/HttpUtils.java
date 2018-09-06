1cd8c6b2c5eac92816420bc27b748137efe25171
yuansong <yuansong6@163.com>
2018-09-06 13:54:32 +0800

    删除多余引用

--- proj/src/main/java/com/yuansong/common/HttpUtils.java
+++ proj/src/main/java/com/yuansong/common/HttpUtils.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.CookieHandler;
-import java.net.CookieManager;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
